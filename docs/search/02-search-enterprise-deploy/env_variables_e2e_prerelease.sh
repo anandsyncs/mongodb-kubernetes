fe326106a875a96d7834348066b6c22b7cc3cbec
Łukasz Sierant <lukasz.sierant@mongodb.com>
2025-08-31 20:31:06 +0200

    Running snippets with prerelease flavor

--- docs/search/02-search-enterprise-deploy/env_variables_e2e_prerelease.sh
+++ docs/search/02-search-enterprise-deploy/env_variables_e2e_prerelease.sh
@@ -1,4 +1,12 @@
 export K8S_CTX="${CLUSTER_NAME}"
 
-export OPERATOR_ADDITIONAL_HELM_VALUES=""
-export OPERATOR_HELM_CHART="${PROJECT_DIR}/helm_chart"
+export PRERELEASE_VERSION="1.4.0-prerelease-68b1a853973bae0007d5eaa0"
+
+export PRERELEASE_IMAGE_PULLSECRET="${COMMUNITY_PRIVATE_PREVIEW_PULLSECRET_DOCKERCONFIGJSON}"
+export OPERATOR_ADDITIONAL_HELM_VALUES="registry.imagePullSecrets=prerelease-image-pullsecret"
+export OPERATOR_HELM_CHART="oci://quay.io/mongodb/staging/mongodb-kubernetes:${PRERELEASE_VERSION}"
+
+export OPS_MANAGER_API_URL="${OM_BASE_URL}"
+export OPS_MANAGER_API_USER="${OM_USER}"
+export OPS_MANAGER_API_KEY="${OM_API_KEY}"
+export OPS_MANAGER_ORG_ID="${OM_ORGID}"
