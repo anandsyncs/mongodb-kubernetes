aea65d6894db420688b09d0dca159e36df0f1b66
Łukasz Sierant <lukasz.sierant@mongodb.com>
2025-09-01 18:13:46 +0200

    wip

--- docs/search/02-search-enterprise-deploy/env_variables_e2e_prerelease.sh
+++ docs/search/02-search-enterprise-deploy/env_variables_e2e_prerelease.sh
@@ -1,10 +1,10 @@
 export K8S_CTX="${CLUSTER_NAME}"
 
-export PRERELEASE_VERSION="1.4.0-prerelease-68b1a853973bae0007d5eaa0"
+export PRERELEASE_VERSION="1.4.0-prerelease-68b5c0bb136a0d0007a4c8a2"
 
 export PRERELEASE_IMAGE_PULLSECRET="${COMMUNITY_PRIVATE_PREVIEW_PULLSECRET_DOCKERCONFIGJSON}"
 export OPERATOR_ADDITIONAL_HELM_VALUES="registry.imagePullSecrets=prerelease-image-pullsecret"
-export OPERATOR_HELM_CHART="oci://quay.io/mongodb/staging/mongodb-kubernetes:${PRERELEASE_VERSION}"
+export OPERATOR_HELM_CHART="oci://quay.io/mongodb/staging/helm-chart/mongodb-kubernetes:${PRERELEASE_VERSION}"
 
 export OPS_MANAGER_API_URL="${OM_BASE_URL}"
 export OPS_MANAGER_API_USER="${OM_USER}"
